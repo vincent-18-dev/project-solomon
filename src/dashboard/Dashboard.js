8e9ded3d8eceb7a84ee713dd72840ffeb5ce4080
vincent-18-dev <154895414+vincent-18-dev@users.noreply.github.com>
2023-12-28 23:35:35 +0530

    get exact data

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,63 +1,61 @@
 import "./Dashboard.css";
 import React, { useState } from "react";
 import { MdOutlineArrowDropDown } from "react-icons/md";
+import Dropdown from "react-multilevel-dropdown";
+
 const Dashboard = ({ data }) => {
-  // console.log("data", data);
   let values = data?.data.filter((item) => item?.Menu_Parent_Level === 0) || [];
   const [openSubMenu, setOpenSubMenu] = useState(null);
   const [submenu, setSubmenu] = useState("");
-  const submenuClick = (val, index) => {
-    let submenu = data?.data?.filter((value) => {
-      if (val.Menu_Id === value.Menu_Parent_Level) {
-        return value.Menu_Caption;
-      }
-    });
+  const submenuClick = (val) => {
+    let submenu = data?.data?.filter(
+      (value) => val.Menu_Id === value.Menu_Parent_Id
+    );
     setSubmenu(submenu);
-    setOpenSubMenu((prevIndex) => (prevIndex === index ? null : index));
+    console.log("submenu", submenu);
   };
-
-  const secondMenu = (items, index) => {
-    let nestedmenu = data?.data?.filter((val) => {
-      if (items.Menu_Id === val.Menu_Parent_Level) {
-        return val.Menu_Caption;
-      }
-    });
-    console.log("nestedmenu",nestedmenu)
+  const secondMenu = (items) => {
+    let nestedmenu = data?.data?.filter(
+      (val) => items.Menu_Id === val.Menu_Parent_Id
+    );
+    setOpenSubMenu(nestedmenu);
   };
   return (
     <>
       <div className="container-dashboard">
         <div className="section-home">
           <div className="menubar-section">
-            <ul className="menu-ul">
-              {values.map((val, index) => {
-                return (
-                  <>
-                    <div
-                      className="menu-data-li"
-                      key={index}
-                      onClick={() => submenuClick(val, index)}
-                    >
-                      <li>{val?.Menu_Caption}</li> <MdOutlineArrowDropDown />
-                    </div>
-                    {openSubMenu === index && (
-                      <ul className="sub-menu">
-                        {submenu?.map((items, index) => {
-                          return (
-                            <li
-                              key={index}
-                              onClick={() => secondMenu(items, index)}
-                            >
-                              {items.Menu_Caption}
-                            </li>
-                          );
-                        })}
-                      </ul>
-                    )}
-                  </>
-                );
-              })}
-            </ul>
+            {values.map((val, index) => {
+              return (
+                <Dropdown
+                  position="right"
+                  onClick={() => submenuClick(val)}
+                  title={val?.Menu_Caption}
+                >
+                  {submenu.length > 0 &&
+                    submenu?.map((items) => {
+                      return (
+                        <Dropdown.Item
+                          onClick={() => secondMenu(items)}
+                          position="bottom"
+                        >
+                          {items.Menu_Caption}
+                          {/* {openSubMenu.map((sub) => {
+                            return (
+                              <Dropdown.Submenu position="bottom">
+                                <Dropdown.Item>
+                                  {sub.Menu_Caption}
+                                </Dropdown.Item>
+                              </Dropdown.Submenu>
+                            );
+                          })} */}
+                        </Dropdown.Item>
+                      );
+                    })}
+                  {/* <Dropdown.Item>Item 2</Dropdown.Item> */}
+                </Dropdown>
+              );
+            })}
           </div>
           <div className="dashboard-section"></div>
         </div>
